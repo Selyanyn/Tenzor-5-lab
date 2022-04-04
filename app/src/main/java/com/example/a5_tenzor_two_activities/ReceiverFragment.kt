73d35fc8efea728e9c9888a8a34669bc55856ab7
Selyanyn <56536978+Selyanyn@users.noreply.github.com>
2022-04-05 00:26:56 +0500

    5-05.04: every stage of fragments lifecycle was logged

--- app/src/main/java/com/example/a5_tenzor_two_activities/ReceiverFragment.kt
+++ app/src/main/java/com/example/a5_tenzor_two_activities/ReceiverFragment.kt
@@ -3,13 +3,17 @@ package com.example.a5_tenzor_two_activities
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class ReceiverFragment : Fragment(R.layout.receiver_fragment) {
-        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Log.i("LifecycleTag", "Receiver: onViewCreated")
         val readButton = view.findViewById<Button>(R.id.read_button)
         val message = view.findViewById<TextView>(R.id.message_text_box)
         message.setText(arguments?.getString("message") ?: "")
@@ -17,5 +21,64 @@ class ReceiverFragment : Fragment(R.layout.receiver_fragment) {
         readButton.setOnClickListener {
             message.setText(getString(R.string.message_was_read))
         }
+    }
+
+    override fun onAttach(context: Context) {
+        Log.i("LifecycleTag", "Receiver: onAttach")
+        super.onAttach(context)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("LifecycleTag", "Receiver: onCreate")
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Log.i("LifecycleTag", "Receiver: onCreateView")
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onStart() {
+        Log.i("LifecycleTag", "Receiver: onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.i("LifecycleTag", "Receiver: onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.i("LifecycleTag", "Receiver: onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.i("LifecycleTag", "Receiver: onStop")
+        super.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.i("LifecycleTag", "Receiver: onSaveInstanceState")
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onDestroyView() {
+        Log.i("LifecycleTag", "Receiver: onDestroyView")
+        super.onDestroyView()
+    }
+
+    override fun onDestroy() {
+        Log.i("LifecycleTag", "Receiver: onDestroy")
+        super.onDestroy()
+    }
+
+    override fun onDetach() {
+        Log.i("LifecycleTag", "Receiver: onDetach")
+        super.onDetach()
     }
 }
--- app/src/main/java/com/example/a5_tenzor_two_activities/SenderFragment.kt
+++ app/src/main/java/com/example/a5_tenzor_two_activities/SenderFragment.kt
@@ -1,14 +1,19 @@
 package com.example.a5_tenzor_two_activities
 
+import android.content.Context
 import android.content.ReceiverCallNotAllowedException
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.google.android.material.textfield.TextInputEditText
 
 class SenderFragment : Fragment(R.layout.sender_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Log.i("LifecycleTag", "Sender: onViewCreated")
         super.onViewCreated(view, savedInstanceState)
         val updateButton = requireView().findViewById<Button>(R.id.send_button)
         val textInput = requireView().findViewById<TextInputEditText>(R.id.text_input)
@@ -24,5 +29,64 @@ class SenderFragment : Fragment(R.layout.sender_fragment) {
                 .addToBackStack(null)
                 .commit()
         }
+    }
+
+    override fun onAttach(context: Context) {
+        Log.i("LifecycleTag", "Sender: onAttach")
+        super.onAttach(context)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("LifecycleTag", "Sender: onCreate")
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Log.i("LifecycleTag", "Sender: onCreateView")
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onStart() {
+        Log.i("LifecycleTag", "Sender: onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.i("LifecycleTag", "Sender: onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.i("LifecycleTag", "Sender: onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.i("LifecycleTag", "Sender: onStop")
+        super.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.i("LifecycleTag", "Sender: onSaveInstanceState")
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onDestroyView() {
+        Log.i("LifecycleTag", "Sender: onDestroyView")
+        super.onDestroyView()
+    }
+
+    override fun onDestroy() {
+        Log.i("LifecycleTag", "Sender: onDestroy")
+        super.onDestroy()
+    }
+
+    override fun onDetach() {
+        Log.i("LifecycleTag", "Sender: onDetach")
+        super.onDetach()
     }
 }
